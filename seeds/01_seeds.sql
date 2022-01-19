ecb4800bc36280ec845af9f3f3b35ff4a154ea0a
Matthew Tran <m.tran906@gmail.com>
2022-01-19 19:39:43 +0000

    add data to each of the tables

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,19 @@
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES ('2018-09-11', '2018-09-26', 1, 1),
+('2019-01-04', '2019-02-01', 2, 2),
+('2021-10-01', '2021-10-14', 3, 3);
+
+INSERT INTO users (name, email, password)
+VALUES ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Louisa Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'); 
+
+INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES (1, 'Speed lamp', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 85234, 2, 6, 7, 'Canada', '651 Roger Rd', 'Bohbatev', 'Alberta', '83680', TRUE ), 
+(1, 'Slow light', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 10025, 0, 1, 2, 'Canada', '111 Foul Rd', 'Prince George', 'British Columbia', '24960', TRUE ), 
+(2, 'MTV CribsAlot', 'description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 56234, 2, 3, 4, 'Canada', '345 Spur St', 'Toronto', 'Ontario', '46993', TRUE );
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES (2, 2, 22, 3, 'messages'), 
+(1, 3, 23, 4, 'messages'), 
+(3, 1, 24, 4, 'messages'); 
