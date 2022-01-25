124f7ff4d600c0ce71312b905cde6357e2efc54c
Matthew Tran <m.tran906@gmail.com>
2022-01-25 18:57:13 +0000

    refactor our getAllProperties function to accept arguments that will enable filtering

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -125,25 +125,55 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 
- const getAllProperties = (options, limit = 10) => {
-  return pool
-    //.query(`SELECT * FROM properties LIMIT $1`, [limit])
-    .query(`SELECT city, owner_id, cost_per_night, avg(property_reviews.rating) as average_rating
+  const getAllProperties = function (options, limit = 10) {
+    // 1
+    const queryParams = [];
+    // 2
+    let queryString = `
+    SELECT properties.*, avg(property_reviews.rating) as average_rating
     FROM properties
     JOIN property_reviews ON properties.id = property_id
-    WHERE city LIKE '%ancouv%'
-    GROUP BY properties.id
-    HAVING avg(property_reviews.rating) >= 4
+    WHERE 1 = 1
+    `;
+  
+    // 3
+    if (options.city) {
+      queryParams.push(`%${options.city}%`);
+      queryString += ` AND city LIKE $${queryParams.length} `;
+    }
+
+    if (options.owner_id) {
+      queryParams.push(`${options.owner_id}`);
+      queryString += `AND owner_id = $${queryParams.length} `;
+    }
+ 
+    if (options.minimum_price_per_night && options.maximum_price_per_night) {
+      queryParams.push(`${options.minimum_price_per_night}`);
+      queryParams.push(`${options.maximum_price_per_night}`);
+      queryString += ` AND (cost_per_night >= $${queryParams.length - 1} AND cost_per_night <= $${queryParams.length}) \n`
+    }
+      queryString += ` GROUP BY properties.id \n`
+
+    if (options.minimum_rating) {
+      queryParams.push(`${options.minimum_rating}`);
+      queryString += `HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+    }
+  
+    // 4
+    queryParams.push(limit);
+    queryString += `
     ORDER BY cost_per_night
-    LIMIT 10;`, [limit])
-    .then((result) => {
-      //console.log(result.rows);
-      return result.rows;
-    })
-    .catch((err) => {
-      console.log(err.message);
-    });
-};
+    LIMIT $${queryParams.length};
+    `;
+  
+    // 5
+    console.log(queryString, queryParams);
+  
+    // 6
+    return pool
+    .query(queryString, queryParams)
+    .then((res) => res.rows);
+  };
 exports.getAllProperties = getAllProperties;
 
 
