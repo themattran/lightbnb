90323c48b820f18daaf47d29e15beaec70c447be
Matthew Tran <m.tran906@gmail.com>
2022-01-26 18:08:10 +0000

    add function for user to create and add their own listing to the database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -97,22 +97,6 @@ const getAllReservations = function(guest_id, limit = 10) {
   .catch((err) => {
     console.log('Query Error', err)
   })
-  // return pool
-  // .query(`SELECT reservations.id
-  // FROM reservations
-  // JOIN properties ON reservations.property_id = properties.id
-  // JOIN property_reviews ON properties.id = property_reviews.property_id
-  // WHERE reservations.guest_id = $1
-  // GROUP BY properties.id, reservations.id
-  // ORDER BY reservations.start_date
-  // LIMIT $2;`, [guest_id, limit])
-  // .then((result) => {
-  //   console.log('whatever', result.rows);
-  //   return result.rows[0];
-  // })
-  // .catch((err) => {
-  //   console.log(err.message);
-  // });
 }
 exports.getAllReservations = getAllReservations;
 
@@ -133,7 +117,7 @@ exports.getAllReservations = getAllReservations;
     SELECT properties.*, avg(property_reviews.rating) as average_rating
     FROM properties
     JOIN property_reviews ON properties.id = property_id
-    WHERE 1 = 1
+    WHERE TRUE 
     `;
   
     // 3
@@ -183,9 +167,15 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  return pool
+  .query(`INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms) 
+          VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14) RETURNING *;`, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms])
+  .then((result) => {
+    console.log(result.rows[0]);
+    return result.rows[0];
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 }
 exports.addProperty = addProperty;
